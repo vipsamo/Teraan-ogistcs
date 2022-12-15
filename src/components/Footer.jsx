122449dbd0885e03eb645297ad616e56277d6d7f
vipsamo <mulujisamuel@gmail.com>
2022-12-16 02:19:34 +0300

    new updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,21 +12,16 @@ const Footer = () => {
           className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
          md:w-2/5"
         >
-          <span className="text-indigo-400">Get</span> in touch
+          <span className="text-indigo-400">Download</span> our brochure
          
         </h1>
         <div>
-          <input
-            type="text"
-            placeholder="email us here"
-            className="text-gray-800
-           sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
-          />
           <button
             className="bg-indigo-400 hover:bg-indigo-500 duration-300 px-5 py-2.5 font-[Poppins]
            rounded-md text-white md:w-auto w-full"
+            
           >
-            Send
+           Download
           </button>
         </div>
       </div>
